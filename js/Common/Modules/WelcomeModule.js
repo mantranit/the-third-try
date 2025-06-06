fd6fcf95e4cc96c36e08608d9b58293067aa29a9
Man Tran <tranduyminhman@gmail.com>
2025-06-06 23:08:05 +0700

    preload video

--- js/Common/Modules/WelcomeModule.js
+++ js/Common/Modules/WelcomeModule.js
@@ -22,7 +22,9 @@ $(function() {
     },
 
     renderPage: function() {
-      $("#app").html('<div id="welcomePage" class="page welcome-page"><div class="video-background"><video width="100%" preload="auto" playsinline autoplay muted loop src="http://103.153.72.195:8080/video/HITEC_Scandic_Video_No_Sound.mp4"></video></div><div class="welcome-wrapper"><h1 id="welcomeTitle">DEAR</h1><div class="welcome"><p id="welcomeBrief">Welcome</p></div><div class="button-continue"><span class="a" id="welcomeContinue">Continue</span></div></div><div class="right-bottom-buttons-wrapper"><div class="right-bottom-buttons"><button><div class="icon" style="background-color:red"></div><div class="text"><span id="buttonTelevision">Television</span></div></button><button><div class="icon" style="background-color:#143f7b"></div><div class="text"><span id="buttonLanguage">Language</span></div></button></div></div><div class="cover"></div></div>');
+      $("#app").html(
+        '<div id="welcomePage" class="page welcome-page"><div class="video-background"></div><div class="welcome-wrapper"><h1 id="welcomeTitle">DEAR</h1><div class="welcome"><p id="welcomeBrief">Welcome</p></div><div class="button-continue"><span class="a" id="welcomeContinue">Continue</span></div></div><div class="right-bottom-buttons-wrapper"><div class="right-bottom-buttons"><button><div class="icon" style="background-color:red"></div><div class="text"><span id="buttonTelevision">Television</span></div></button><button><div class="icon" style="background-color:#143f7b"></div><div class="text"><span id="buttonLanguage">Language</span></div></button></div></div><div class="cover"></div></div>'
+      );
     },
   };
 
